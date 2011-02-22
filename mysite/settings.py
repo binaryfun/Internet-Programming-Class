bd60b451471ed6cf278dd7e6760d579c99cfde50
binaryfun <directorymanager@gmail.com>
2011-02-22 01:14:58 +0000

    committing all

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,6 +81,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+"/home/bazar/python-scripts/Internet-Programming-Class/mysite/templates",
 )
 
 INSTALLED_APPS = (
@@ -91,7 +92,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'polls',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+ 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
